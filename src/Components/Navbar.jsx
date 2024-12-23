50fbf539e8941b7e7ce158ac2002d174d94fd044
Jenil Savalia <jenil.savalia.cd@gmail.com>
2024-12-23 10:06:07 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,13 +6,13 @@ function Navbar() {
     return (
 
         <>
-            {/* <div className='text-[1.8rem] bg-red-500 flex list-none'>
+             <div className='text-[1.8rem] bg-red-500 flex list-none'>
                 <Link to='/mealdb' className="pr-10">mealdb</Link>
                 <Link to='/cocktaildb' className="pr-10">cocktailbd</Link>
                 <Link to='/harrypotterdb' className="pr-10">harrypotter</Link>
                 <Link to='/bankdb' className="pr-10">bankdb</Link>
                 <Link to='/' className="pr-10">About us</Link>
-            </div> */}
+            </div> 
             <Outlet />
         </>
 
